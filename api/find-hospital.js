5fb35d0b66f4a4090921b45dfb75970fad16a5f5
osman-koc <kocosm@hotmail.com>
2025-06-23 14:59:34 +0300

    fix: json error & update domain

--- api/find-hospital.js
+++ api/find-hospital.js
@@ -1,12 +1,20 @@
 import 'dotenv/config';
 import geolib from 'geolib';
 import queryString from 'query-string';
-import hospitalsData from '../db/hospitals.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Function to get user location and find pharmacies by province and district
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Function to get user location and find hospitals by province and district
 async function findHospitalsFromDb(city, district, userLocation) {
     const upperCaseCity = city.toLocaleUpperCase('tr');
     const upperCaseDistrict = district.toLocaleUpperCase('tr');
+
+    // Load hospitals data dynamically with correct path
+    const hospitalsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/hospitals.json'), 'utf-8'));
 
     // Filter hospitals by province and district
     const filteredHospitals = hospitalsData.filter(hospital =>
@@ -14,7 +22,7 @@ async function findHospitalsFromDb(city, district, userLocation) {
         hospital.district.toLocaleUpperCase('tr') === upperCaseDistrict
     );
 
-    // Calculate the distance from the centre for each pharmacy and sort
+    // Calculate the distance from the centre for each hospital and sort
     const hospitalsWithDistances = filteredHospitals.map(hospital => {
         const hospitalLocation = {
             latitude: parseFloat(hospital.location.lat),
